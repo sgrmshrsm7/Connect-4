61397a7a719ba8a7bb6f7018f340505bb3ee133a
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-08-03 16:32:54 +0530

    Added names of players :book:

--- 4inArow.py
+++ 4inArow.py
@@ -1,3 +1,13 @@
 board = [[' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' '] , [' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' '] , [' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' '] , [' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' '] , [' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' '] , [' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' '] , [' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ']]
 
 uplayer = [6 , 6 , 6 , 6 , 6 , 6 , 6]
+
+quit = False
+
+while quit == False:
+    name1 = str(raw_input("Enter name of person_1('O') : "))
+    name2 = str(raw_input("Enter name of person_2('X') : "))
+    
+    choice = str(raw_input("Enter q for quit : "))
+    if choice == 'q':
+        quit = True
