bf20f4e39bad426af72de972f68467116fb8194e
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-08-03 17:06:15 +0530

    Updating board

--- 4inArow.py
+++ 4inArow.py
@@ -9,7 +9,7 @@ while quit == False:
 
     name1 = str(raw_input("Enter name of person_1(O) : "))
     name2 = str(raw_input("Enter name of person_2(X) : "))
-    name1 += '(0)'
+    name1 += '(O)'
     name2 += '(X)'
 
     turn = 0
@@ -18,8 +18,10 @@ while quit == False:
     while win == False:
         if turn % 2 == 0:
             name = name1
+            char = 'O'
         else:
             name = name2
+            char = 'X'
 
         value = input('\n ' + name + '\'s turn : ')
 
@@ -29,6 +31,7 @@ while quit == False:
             if uplayer[value] < 0:
                 print('\n Invalid Entry : ')
             else:
+                board[value][uplayer[value]] = char
 
     if win == True:
         if turn % 2 == 0:
