4958d9be4acae5b22b8e82c6f2cdd9c379e754b8
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-08-03 16:42:58 +0530

    Win Print

--- 4inArow.py
+++ 4inArow.py
@@ -7,7 +7,21 @@ quit = False
 while quit == False:
     name1 = str(raw_input("Enter name of person_1('O') : "))
     name2 = str(raw_input("Enter name of person_2('X') : "))
-    
+
+    turn = 0
+    win = False
+
+    while win == False:
+        pass
+
+    if win == True:
+        if turn % 2 == 0:
+            print('\n ' + name1 + 'won the match !!!\n')
+        else:
+            print('\n ' + name2 + 'won the match !!!\n')
+    else:
+        print("\n Match drawn !!!\n")
+        
     choice = str(raw_input("Enter q for quit : "))
     if choice == 'q':
         quit = True
