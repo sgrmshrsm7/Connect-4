7569fc9803b17163d0193ebce049ba7f5a8b056c
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-08-03 18:48:02 +0530

    board print modify

--- 4inArow.py
+++ 4inArow.py
@@ -23,7 +23,13 @@ while quit == False:
             name = name2
             char = 'X'
 
-        print(board)
+        print(board[0])
+        print(board[1])
+        print(board[2])
+        print(board[3])
+        print(board[4])
+        print(board[5])
+        print(board[6])
 
         value = input('\n ' + name + '\'s turn : ')
 
